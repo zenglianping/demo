92b57596f70be1e5fd0687b1e48d4686cae2b3a3
zenglianping <leopold_zlp@126.com>
2020-03-11 10:20:41 +0800

    SortDemo

--- src/main/java/com/example/demo/datastructure/SortDemo.java
+++ src/main/java/com/example/demo/datastructure/SortDemo.java
@@ -1,36 +1,55 @@
 package com.example.demo.datastructure;
 
+import java.util.Arrays;
+
 public class SortDemo {
 
     public static void main(String[] args) {
 
+        int[] arr = new int[]{6, 1, 2, 5, 4, 3, 9, 7, 10, 8};
+
+        SortDemo sortDemo = new SortDemo();
+
+        sortDemo.quickSort(arr, 0, arr.length - 1);
+
+        System.out.println(Arrays.toString(arr));
+
     }
 
-    private void quickSort(int[] arr) {
-        int len = arr.length;
-        int left =0, right=len-1 , key = arr[len/2] ;
+    //从小到大排序
+    private void quickSort(int[] arr, int start, int end) {
+        if (start > end) { // 中断递归
+            return;
+        }
+        int key = arr[start]; // 用于比较的关键字
+        int left = start;
+        int right = end;
         while (left < right) {
-            for (int i = right ; i >= left; i--) {
-                if (arr[i] > key) {
-                    right--;
-                    break;
-                }
+            //从右往左
+            while (arr[right] >= key && left < right) {
+                right--;
             }
 
-            for (int i = left; i < right ; i++ ) {
-                if (arr[i] < key) {
-                    left++;
-                    break;
-                }
+            //从左往右
+            while (arr[left] <= key && left < right) {
+                left++;
             }
 
-            if (left != right) {
+            if (left < right) { //交换位置
                 int temp = arr[left];
                 arr[left] = arr[right];
                 arr[right] = temp;
             }
         }
 
+        //基准位置与分隔位互换
+        arr[start] = arr[left];
+        arr[left] = key;
+
+        quickSort(arr, start, left - 1);
+        quickSort(arr, left + 1, end);
 
     }
+
+
 }
